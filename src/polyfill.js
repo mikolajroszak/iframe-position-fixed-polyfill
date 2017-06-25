d8b6498ce1ee1a9d112ab899992931378bf6e643
Julian Xhokaxhiu <xhokaxhiujulian@gmail.com>
2017-06-25 14:04:55 +0200

    Add support for dynamic appended elements
    The polyfill will recognize them, and it will initialize only elements that were not already initialized

--- src/polyfill.js
+++ src/polyfill.js
@@ -1,6 +1,12 @@
 (function(window){
   // Initialize this polyfill when the document has been fully loaded, including CSS
   window.addEventListener('load', initPolyfill, false);
+  // Observe for new added elements
+  var observer = new MutationObserver(initPolyfill);
+  observer.observe(document.documentElement, {
+      childList: true,
+      subtree: true
+  });
   // -------------------------------------------------------------------
   function loop ( elements, callback ) {
     for ( var i = 0; i < elements.length; i++ ) {
@@ -21,26 +27,30 @@
   }
   // -------------------------------------------------------------------
   function iFramePositionFixPolyfill ( element, initialStyles ) {
-    var me = this;
+    var me = this,
+        isInitialized = ( element.getAttribute('data-iframe-position-fixed-initialized') === "true" );
 
-    // Define properties
-    me.element = element;
-    me.initialTop = initialStyles.top.indexOf('%') > -1 ? ( window.parent.innerHeight / 100 * initialStyles.top.slice(0,-1) ) : element.offsetTop;
-    me.updateTimeout = null;
+    if ( !isInitialized ) {
+      // Define properties
+      me.element = element;
+      me.initialTop = initialStyles.top.indexOf('%') > -1 ? ( window.parent.innerHeight / 100 * initialStyles.top.slice(0,-1) ) : element.offsetTop;
+      me.updateTimeout = null;
 
-    // Define internal functions
-    me.update = function ( scrollTop ) {
-      me.element.style.top = me.initialTop + ( scrollTop >= window.frameElement.offsetTop ? scrollTop - window.frameElement.offsetTop : scrollTop ) + 'px';
+      // Define internal functions
+      me.update = function ( scrollTop ) {
+        me.element.style.top = me.initialTop + ( scrollTop >= window.frameElement.offsetTop ? scrollTop - window.frameElement.offsetTop : scrollTop ) + 'px';
+      }
+
+      // Initialize element position
+      me.element.style.top = me.initialTop + 'px';
+      me.element.setAttribute( 'data-iframe-position-fixed-initialized', true );
+
+      // When the parent scrolls, attempt to update the element position ever 500ms.
+      // If another scroll event comes in place, cancel the previous attempt and retry.
+      window.parent.addEventListener( 'scroll', function ( event ){
+        if ( me.updateTimeout ) clearTimeout( me.updateTimeout );
+        me.updateTimeout = setTimeout( function(){ me.update( window.parent.pageYOffset ) }, 100 );
+      }, true)
     }
-
-    // Initialize element position
-    me.element.style.top = me.initialTop + 'px';
-
-    // When the parent scrolls, attempt to update the element position ever 500ms.
-    // If another scroll event comes in place, cancel the previous attempt and retry.
-    window.parent.addEventListener( 'scroll', function ( event ){
-      if ( me.updateTimeout ) clearTimeout( me.updateTimeout );
-      me.updateTimeout = setTimeout( function(){ me.update( window.parent.pageYOffset ) }, 100 );
-    }, true)
   }
 })(window);
